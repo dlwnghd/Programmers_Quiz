2296ea537d92f80931bfbffa74ccec2e86fad214
Lee Juhong <alion0513@naver.com>
2022-02-17 22:31:38 +0900

    2022-02-17

---
+++ Level1/src/level1/Change_the_string_to_an_integer.java
@@ -0,0 +1,13 @@
+package level1;
+
+public class Change_the_string_to_an_integer {
+	public static void main(String[] args) {
+		class Solution {
+		    public int solution(String s) {
+		        int answer = 0;
+		        answer = Integer.parseInt(s);
+		        return answer;
+		    }
+		}
+	}
+}
