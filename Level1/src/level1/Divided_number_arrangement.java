35df46dc119adbc92442dbcad00174ca63c98dcd
Lee Juhong <alion0513@naver.com>
2022-02-28 22:11:25 +0900

    2022-02-28

--- Level1/src/level1/Divided_number_arrangement.java
+++ Level1/src/level1/Divided_number_arrangement.java
@@ -1,6 +1,8 @@
 package level1;
 
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Divided_number_arrangement {
 	public static void main(String[] args) {
@@ -10,33 +12,46 @@ public class Divided_number_arrangement {
 				int[] answer = null;
 				int count = 0;
 
+				Arrays.sort(arr);
+
+				// 1. 배열내에서 중복되는 수를 제거
+				ArrayList<Integer> arrayList = new ArrayList<>();
+
+				for (int item : arr) {
+					if (!arrayList.contains(item))
+						arrayList.add(item);
+				}
+
 				// 나누어 지는 수의 갯수 구하기
-				for (int i = 0; i < arr.length; i++) {
-					if (arr[i] % divisor == 0) {
+				for (int i : arrayList) {
+					if (i % divisor == 0) {
 						count++;
 					}
 				}
 
-				// answer에 나누어 지는 수만큼 방 생성
-				answer = new int[count];
-
 				// 만약 나누어 지는 수가 없을 경우
-				if (answer.length == 0) {
+				if (count == 0) {
 					answer = new int[1];
 					answer[0] = -1;
 					return answer;
+				} else {
+					// answer에 나누어 지는 수만큼 방 생성
+					answer = new int[count];
 				}
 
+				int k = 0;
 				// 나누어 지는 수들을 answer에 넣어줌
 				for (int i = 0; i < count; i++) {
-					for (int j = i; j < arr.length; j++) {
-						if (arr[j] % divisor == 0) {
-							answer[i] = arr[j];
+					for (int j = k; j < arr.length; j++) {
+						if (arrayList.get(j) % divisor == 0) {
+							answer[i] = arrayList.get(j);
+							k = j + 1;
 							break;
 						}
 					}
 				}
-				Arrays.sort(answer);
+
+				System.out.println(arrayList);
 
 				return answer;
 			}
---
+++ Level1/src/level1/I_don_t_like_the_same_number.java
@@ -0,0 +1,16 @@
+package level1;
+
+import java.util.*;
+
+public class I_don_t_like_the_same_number {
+	public class Solution {
+	    public int[] solution(int []arr) {
+	        int[] answer = {};
+	        
+	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+	        System.out.println("Hello Java");
+
+	        return answer;
+	    }
+	}
+}
