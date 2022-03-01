499144026c941706075d9c66d00fb4a51b6cc95d
Lee Juhong <alion0513@naver.com>
2022-03-01 14:58:01 +0900

    2022-03-01

--- Level1/src/level1/I_don_t_like_the_same_number.java
+++ Level1/src/level1/I_don_t_like_the_same_number.java
@@ -4,13 +4,28 @@ import java.util.*;
 
 public class I_don_t_like_the_same_number {
 	public class Solution {
-	    public int[] solution(int []arr) {
-	        int[] answer = {};
-	        
-	        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-	        System.out.println("Hello Java");
+		public int[] solution(int[] arr) {
+			int[] answer = {};
+			int answer_length = 0, count = 0;
 
-	        return answer;
-	    }
+			for (int i = 0; i < arr.length; i++) {
+				if (i + 1 < arr.length && arr[i] == arr[i + 1]) {
+					continue;
+				}
+				answer_length++;
+			}
+
+			answer = new int[answer_length];
+
+			for (int i = 0; i < arr.length; i++) {
+				if (i + 1 < arr.length && arr[i] == arr[i + 1]) {
+					continue;
+				}
+				answer[count] = arr[i];
+				count++;
+			}
+
+			return answer;
+		}
 	}
 }
